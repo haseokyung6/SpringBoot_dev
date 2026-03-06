31897438800be96b798ecc3cef6ebfe8aad26713
haseokyung6 <haseokyung@naver.com>
2026-03-06 16:26:42 +0900

    Runner update

--- src/main/java/com/basic/dev/runner/MyRunner.java
+++ src/main/java/com/basic/dev/runner/MyRunner.java
@@ -1,14 +1,24 @@
 package com.basic.dev.runner;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MyRunner implements ApplicationRunner {
+    @Value("${myboot.name}")
+    private String name;
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        System.out.println("vm arguement foo: "+ args.containsOption("foo"));
-        System.out.println("program arguement bar: "+ args.containsOption("bar"));
+        System.out.println("${myboot.nam}  = " + name);
+
+        System.out.println("VM Arguement foo : " + args.containsOption("foo"));
+        System.out.println("Program Arguement bar : " + args.containsOption("bar"));
+
+        //Argument 목록 출력하기
+        args.getOptionNames()//Set<String>
+                .forEach(name -> System.out.println(name));
     }
 }
