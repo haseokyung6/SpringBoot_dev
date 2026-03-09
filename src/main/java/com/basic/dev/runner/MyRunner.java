e240d7fd10c324f8a077889a77be3bc8dbfebdce
haseokyung6 <haseokyung@naver.com>
2026-03-09 10:42:08 +0900

    Config Folder add

--- src/main/java/com/basic/dev/runner/MyRunner.java
+++ src/main/java/com/basic/dev/runner/MyRunner.java
@@ -1,14 +1,23 @@
 package com.basic.dev.runner;
 
+import com.basic.myspringboot.config.CustomerVO;
+import com.basic.myspringboot.property.MyBootProperties;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
+
+import java.util.function.Consumer;
 
 @Component
 public class MyRunner implements ApplicationRunner {
     @Value("${myboot.name}")
     private String name;
+
+    @Autowired
+    private CustomerVO customerVO;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
@@ -17,6 +26,14 @@ public class MyRunner implements ApplicationRunner {
         System.out.println("VM Arguement foo : " + args.containsOption("foo"));
         System.out.println("Program Arguement bar : " + args.containsOption("bar"));
 
+        //Anonymous Inner Class
+        args.getOptionNames()
+                .forEach(new Consumer<String>() {
+                    @Override
+                    public void accept(String s) {
+                        System.out.println(s);
+                    }
+                });
         //Argument 목록 출력하기
         args.getOptionNames()//Set<String>
                 .forEach(name -> System.out.println(name));
