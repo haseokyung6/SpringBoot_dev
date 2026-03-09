32f8bf204d43904f9b74fd6e16cff53eb3fcc2c3
haseokyung6 <haseokyung@naver.com>
2026-03-09 14:32:51 +0900

    SpringBoot 실습 1 Code Update

--- src/main/java/com/basic/dev/runner/MyRunner.java
+++ src/main/java/com/basic/dev/runner/MyRunner.java
@@ -1,18 +1,19 @@
 package com.basic.dev.runner;
 
 import com.basic.dev.config.CustomerVO;
-import com.basic.dev.property.MyBootProperties;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 import java.util.function.Consumer;
 
+@Slf4j
 @Component
 public class MyRunner implements ApplicationRunner {
+
     @Value("${myboot.name}")
     private String name;
 
@@ -21,21 +22,29 @@ public class MyRunner implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        System.out.println("${myboot.nam}  = " + name);
+        log.info("myboot.name = {}", name);
 
-        System.out.println("VM Arguement foo : " + args.containsOption("foo"));
-        System.out.println("Program Arguement bar : " + args.containsOption("bar"));
+        log.debug("VM Argument foo : {}", args.containsOption("foo"));
+        log.info("Program Argument bar : {}", args.containsOption("bar"));
 
-        //Anonymous Inner Class
+        if (customerVO != null) {
+            log.debug("customerVO = {}", customerVO);
+            log.info("customerVO loaded successfully");
+        } else {
+            log.warn("customerVO is null");
+        }
+
+        // Anonymous Inner Class
         args.getOptionNames()
                 .forEach(new Consumer<String>() {
                     @Override
                     public void accept(String s) {
-                        System.out.println(s);
+                        log.debug("option name = {}", s);
                     }
                 });
-        //Argument 목록 출력하기
-        args.getOptionNames()//Set<String>
-                .forEach(name -> System.out.println(name));
+
+        // Argument 목록 출력하기
+        args.getOptionNames()
+                .forEach(optionName -> log.info("argument option = {}", optionName));
     }
 }
