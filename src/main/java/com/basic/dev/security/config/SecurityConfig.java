609934f257d9c280de3f07a932bb9fbccd4c5c35
haseokyung6 <haseokyung@naver.com>
2026-03-24 14:30:49 +0900

    실습2-4 인증 권한 추가

--- src/main/java/com/basic/dev/security/config/SecurityConfig.java
+++ src/main/java/com/basic/dev/security/config/SecurityConfig.java
@@ -24,6 +24,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/userinfos/**").permitAll()
+                        .requestMatchers("/api/students/**").permitAll()
                         .requestMatchers("/api/**").authenticated()
                         .anyRequest().permitAll()
                 )
